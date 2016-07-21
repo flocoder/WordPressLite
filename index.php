0f3dc4e91452425fd0ac279b85a0d88a4e219317
FloCoder8 <flocoder@gmail.com>
2016-07-21 10:07:48 +1000

    Updated

--- index.php
+++ index.php
@@ -1,31 +1,6 @@
 <?php
 
 /**
-* Gets the absolute URL of the domain
-*/
-function url_origin( $s, $use_forwarded_host = false )
-{
-    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
-    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
-    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
-    $port     = $s['SERVER_PORT'];
-    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
-    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
-    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
-    return $protocol . '://' . $host;
-}
-
-/**
-* Gets full URL 
-*/
-function full_url( $s, $use_forwarded_host = false )
-{
-    return url_origin( $s, $use_forwarded_host ) . $s['REQUEST_URI'];
-}
-
-$absolute_url = full_url( $_SERVER );
-
-/**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
@@ -42,13 +17,33 @@ define('WP_USE_THEMES', true);
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/app/wp-blog-header.php' );
 
-global $wpdb;
 
-$site_url=get_site_url();
-$home_url=get_home_url();
-
-/** Checks if the database is sync with the absolute URL. */
-if(!$home_url==$absolute_url){
-	$wpdb->query($wpdb->prepare("UPDATE $wpdb->options SET option_value = %s WHERE option_name = %s", array($absolute_url, 'home') ));
-	$wpdb->query($wpdb->prepare("UPDATE $wpdb->options SET option_value = %s WHERE option_name = %s", array($absolute_url.'app', 'siteurl') ));	
+/**
+* Below is the custom code to change the URL in the database 
+*/
+function url_origin( $s, $use_forwarded_host = false )
+{
+    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+    $port     = $s['SERVER_PORT'];
+    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+    return $protocol . '://' . $host;
 }
+
+function full_url( $s, $use_forwarded_host = false )
+{
+    return url_origin( $s, $use_forwarded_host ) . $s['REQUEST_URI'];
+}
+
+$absolute_url = full_url( $_SERVER );
+
+if(file_exists('installed.lock')){exit();}
+
+global $wpdb;
+$wpdb->query($wpdb->prepare("UPDATE $wpdb->options SET option_value = %s WHERE option_name = %s", array($absolute_url, 'home') ));
+$wpdb->query($wpdb->prepare("UPDATE $wpdb->options SET option_value = %s WHERE option_name = %s", array($absolute_url.'app', 'siteurl') ));
+
+fopen("installed.lock", "w");
